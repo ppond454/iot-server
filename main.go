b871289141ac0dc6e6632cee6b8612334705db78
w <36293421+ppond454@users.noreply.github.com>
2025-01-27 19:53:55 +0700

    testing

--- main.go
+++ main.go
@@ -2,29 +2,55 @@ package main
 
 import (
 	"fmt"
+	"time"
 
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/ppond454/iot-backend/device"
 )
 
 func main() {
-	controllers := device.NewController()
+	controller, err := device.NewController("192.168.1.100:1883")
 
-	device1 := device.New("device1", "Smart Switch")
-	controllers.AddDevice("device1", device1)
-
-	device2 := device.New("device2", "Smart Thermostat")
-	controllers.AddDevice("device2", device2)
-
-	pingResult, err := device1.Ping()
 	if err != nil {
-		fmt.Printf("Ping failed for device1: %v\n", err)
-	} else {
-		fmt.Printf("Ping result for device1: %.2f\n", pingResult)
+		fmt.Println("Error: ", err)
+		return
+	}
+	if err := controller.Connect(); err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	controller.CheckAliveWorker(time.Second * 2)
+	test()
+
+	select {}
+
+}
+
+func test() {
+	opts := mqtt.NewClientOptions().AddBroker("192.168.1.100:1883")
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		return
+
 	}
 
-	isConnected := device1.IsConnected()
-	fmt.Printf("Device1 is connected: %v\n", isConnected)
+	client.Subscribe("device/pairing", 0, func(c mqtt.Client, m mqtt.Message) {
+		// fmt.Printf("Received message: [%s] %s\n", m.Topic(), m.Payload())
+		go func() {
+			for i := 0; i < 2; i++ {
+				token := client.Publish("device/paired", 0, false, fmt.Sprintf("device%d", i))
+				token.Wait()
+				if token.Error() != nil {
+					fmt.Printf("Error publishing to topic: %v\n", token.Error())
+				}
 
-	deviceData := device1.GetData()
-	fmt.Printf("Device1 data: %+v\n", deviceData)
+			}
+		}()
+		time.Sleep(time.Second * 3)
+		client.Unsubscribe("device/pairing")
+		time.Sleep(time.Second * 3)
+
+		test()
+	})
+
 }
